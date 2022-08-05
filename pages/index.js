61c1903aaab904dc0b07e5f5700686b410fcdde4
C K <cl-ki@web.de>
2022-08-05 13:50:29 +0200

    add styled-comps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
 import StyledButton from "../src/components/StyledButton";
 
 export default function Home() {
