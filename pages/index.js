4c20e6205df9f249f35526cc160b2e0921bc3ec4
C K <cl-ki@web.de>
2022-08-05 13:57:25 +0200

    add minimum styled and changes to button-button finish

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import Image from "next/image";
-
 import StyledButton from "../src/components/StyledButton";
+import StyledWrapper from "../src/components/StyledWrapper";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <StyledWrapper>
       <Head>
         <title>Lean Coffee</title>
         <meta name="description" content="Lean Coffee Board by me" />
       </Head>
       <StyledButton>Add card</StyledButton>
-    </div>
+    </StyledWrapper>
   );
 }
---
+++ src/components/StyledWrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  padding: 50px;
+  background-color: lightsalmon;
+  height: 100vh;
+`;
+
+export default StyledWrapper;
