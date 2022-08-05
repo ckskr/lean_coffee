db093bf4e5b7d214d70b33fe06e2635521b1bb43
C K <cl-ki@web.de>
2022-08-05 13:48:29 +0200

    add button

---
+++ src/components/StyledButton.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: solid grey 1px;
+  padding: 10px 20px;
+  &:hover {
+    background-color: lightsalmon;
+  }
+`;
+
+export default StyledButton;
